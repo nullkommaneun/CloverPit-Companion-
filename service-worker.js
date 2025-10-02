4db5ed850f2b206e6dee01a7954fd0565beeb658
nullkommaneun <thom.ehr@gmail.com>
2025-10-02 18:33:14 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='2025-10-02-09';
+const VERSION='2025-10-02-10';
 self.addEventListener('install', e=>{self.skipWaiting();});
 self.addEventListener('activate', e=>{self.clients.claim();});
 importScripts(`sw-${VERSION}.js`);
